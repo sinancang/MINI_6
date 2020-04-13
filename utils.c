b39183c02c6efe584595a65614f10dd381c8d95b
Sinan Can Gulan <57106474+seejeegee@users.noreply.github.com>
2020-04-13 18:14:30 +0300

    Create utils.c

---
+++ utils.c
@@ -0,0 +1,14 @@
+/* utils.c
+ *
+ * Utility file for polynomial evaluation application.
+ * Does parsing and computation.
+ * Author: Sinan Can Gulan
+ * Department of Computer Science
+ * History:
+ * 13/4/2020: File created, function headers added.
+ */
+int parse(char str[], int coeff, int expo); //parses csv file data
+int powi(int x, int exponent); //computes x to the power of exponent
+
+int main(int argc, char *argv[]){
+}
