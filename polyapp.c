941903f5e131b593aceb8bbb9cd92f98e0f2373c
Sinan Can Gulan <57106474+seejeegee@users.noreply.github.com>
2020-04-13 18:12:47 +0300

    Create polyapp.c
    File initialized

---
+++ polyapp.c
@@ -0,0 +1,15 @@
+/* polyapp.c
+ * Polynomial Evaluation App
+ * -------------------------
+ * This is the main file for an application that constructs a polynomial and evaluates it for certain predetermined values. 
+ *
+ * Author: Sinan Can Gulan
+ * Department of Computer Science
+ * History:
+ * 13/4/2020: File created, function headers added.
+ */
+int main(int argc, char *argv[]){
+// get file name from argument
+// use fgets to read one line at a time
+//
+}
