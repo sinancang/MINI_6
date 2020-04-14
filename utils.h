d10308d5531e70542610603d8957f9a7609101d2
Sinan Can Gulan <57106474+seejeegee@users.noreply.github.com>
2020-04-14 23:03:08 +0300

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,10 @@
+/*
+ * Header file so that files poly.c and polyapp.c can both use the following functions
+ *
+ * Author: Sinan Can Gulan
+ * Department of Computer Science
+ * History:
+ * 14/3/2020: Created and finalized
+ */
+int parse(char str[], int *coeff, int *expo);
+int powi(int *x, int exponent);
