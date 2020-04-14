962011ace810c5f09bf1f7a42a1b069b5544d797
Sinan Can Gulan <57106474+seejeegee@users.noreply.github.com>
2020-04-14 18:02:20 +0300

    add fct parse & powi

--- utils.c
+++ utils.c
@@ -7,8 +7,41 @@
  * History:
  * 13/4/2020: File created, function headers added.
  */
-int parse(char str[], int coeff, int expo); //parses csv file data
-int powi(int x, int exponent); //computes x to the power of exponent
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+int parse(char str[], int *coeff, int *expo); //parses csv file data
+int powi(int *x, int exponent); //computes x to the power of exponent
 
 int main(int argc, char *argv[]){
+        int coeff;
+        int expo;
+        char string[] = "10 20";
+        parse(string, &coeff, &expo);
+
+        int x = 2;
+        int exponent = 0;
+
+        powi(&x, exponent);
+        printf("%d\n", x);
+}
+
+int parse(char str[], int *coeff, int *expo) {
+        char * token = strtok(str, " ");
+        *coeff = atoi(token);
+        token = strtok(NULL, " ");
+        *expo = atoi(token);
+        return 0;
+}
+
+int powi(int *x, int exponent){
+        int power_of = *x;
+        int i = 1;
+        *x = 1;
+        while (i <= exponent){
+                *x *= power_of;
+                i++;
+        }
+
+        return 0;
 }
