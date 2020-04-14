750ab1e4bd3ea11a19d8e64f344087b58461beb8
Sinan Can Gulan <57106474+seejeegee@users.noreply.github.com>
2020-04-14 23:06:30 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,3 +1,4 @@
+
 /* utils.c
  *
  * Utility file for polynomial evaluation application.
@@ -11,38 +12,22 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-int parse(char str[], int *coeff, int *expo); //parses csv file data
-int powi(int *x, int exponent); //computes x to the power of exponent
-
-int main(int argc, char *argv[]){
-        int coeff;
-        int expo;
-        char string[] = "10 20";
-        parse(string, &coeff, &expo);
-
-        int x = 2;
-        int exponent = 0;
-
-        powi(&x, exponent);
-        printf("%d\n", x);
-}
-
-int parse(char str[], int *coeff, int *expo) {
-        char * token = strtok(str, " ");
-        *coeff = atoi(token);
-        token = strtok(NULL, " ");
-        *expo = atoi(token);
-        return 0;
+int parse(char str[], int *coeff, int *expo){
+	char * token = strtok(str, " ");
+	*coeff = atoi(token);
+	token = strtok(NULL, " ");
+	*expo = atoi(token);
+	return 0;
 }
 
 int powi(int *x, int exponent){
-        int power_of = *x;
-        int i = 1;
-        *x = 1;
-        while (i <= exponent){
-                *x *= power_of;
-                i++;
-        }
-
-        return 0;
+	int power_of = *x;
+	int i = 1;
+	*x = 1;
+	while (i <= exponent){
+		*x *= power_of;
+		i++;
+	}
+	
+	return 0;
 }
